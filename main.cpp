6fd016155c7eb7cca47cf78cc6b5e26f4a930579
Petr Semiletov <peter.semiletov@gmail.com>
2023-01-12 17:01:22 +0300

    cycle init

--- main.cpp
+++ main.cpp
@@ -88,7 +88,7 @@ void CFPrinfWriter::write (const string &s)
 }
 
 
-
+/*
 int main (int argc, char *argv[])
 {
 //  cout << "version: " << VERSION_NUMBER << endl;
@@ -100,14 +100,6 @@ int main (int argc, char *argv[])
   std::signal (SIGINT, signal_handler);
 
 
-/*
-Params initialization order and overrides:
-
-1. config file
-2. command line parameters
-3. ENV variables
-
-*/
 
 //Try to load params from config
 
@@ -274,7 +266,7 @@ Params initialization order and overrides:
         cout << "size_needed, bytes: " << size_needed << endl;
 
 
-     if (size_needed >= free_space/*sinfo.available*/ )
+     if (size_needed >= free_space )
        {
         //exit from program
 
@@ -282,10 +274,6 @@ Params initialization order and overrides:
         return 0;
        }
     }
-
-/*******************************
-MAIN LOOP
-******************************/
 
 
    auto start = high_resolution_clock::now();
@@ -352,3 +340,138 @@ MAIN LOOP
 
   return 0;
 }
+*/
+
+
+int main (int argc, char *argv[])
+{
+//  cout << "version: " << VERSION_NUMBER << endl;
+
+
+
+  vector <string> envars = {"LFG_DURATION", "LFG_RATE", "LFG_LOGFILE", "LFG_TEMPLATEFILE", "LFG_DEBUG", "LFG_PURE"};
+
+  CParameters params;
+
+//  std::signal (SIGINT, signal_handler);
+
+//Try to load params from config
+
+
+  string fname_config = "/etc/logfilegen/logfilegen.conf";
+
+  if (! file_exists (fname_config))
+      fname_config = get_home_dir() + "/.config/logfilegen/logfilegen.conf";
+
+  if (! file_exists (fname_config))
+      fname_config = current_path() + "/logfilegen.conf";
+
+
+//   cout << "Load parameters from config file: " << fname_config << endl;
+
+   //load params from config:
+   CPairFile opts_config (fname_config);
+
+   params.duration = opts_config.get_int ("duration", 2);
+   params.rate = opts_config.get_int ("rate", 3);
+   params.logfile = opts_config.get_string ("logfile", "stdout");
+   params.templatefile = opts_config.get_string ("templatefile", "NOTEMPLATEFILE");
+   params.mode = opts_config.get_string ("mode", "nginx");
+   params.pure = opts_config.get_bool ("pure", false);
+   params.debug = opts_config.get_bool ("debug", false);
+
+
+   if (params.debug)
+      params.print();
+
+// Load params from command line
+
+   if (params.debug)
+      cout << "Load params from command line" << endl;
+
+
+  CPairFile opts_cmdline (argc, argv);
+
+  params.duration = opts_cmdline.get_int ("duration", params.duration);
+  params.rate = opts_cmdline.get_int ("rate", params.rate);
+  params.logfile = opts_cmdline.get_string ("logfile", params.logfile);
+  params.templatefile = opts_cmdline.get_string ("templatefile", params.templatefile);
+  params.mode = opts_cmdline.get_string ("mode", params.mode);
+  params.pure = opts_cmdline.get_bool ("pure", params.pure);
+
+
+  //params.print();
+
+// load params from ENV
+
+  if (params.debug)
+     cout << "Load ENV" << endl;
+
+
+  CPairFile opts_envars (envars);
+
+  params.duration = opts_envars.get_int ("duration", params.duration);
+  params.rate = opts_envars.get_int ("rate", params.rate);
+  params.logfile = opts_envars.get_string ("logfile", params.logfile);
+  params.templatefile = opts_envars.get_string ("templatefile", params.templatefile);
+  params.mode = opts_envars.get_string ("mode", params.mode);
+  params.pure = opts_envars.get_bool ("pure", params.pure);
+
+  //params.print();
+
+
+  if (params.logfile == "stdout")
+     params.bstdout = true;
+  else
+      params.bstdout = false;
+
+  if (params.logfile[0] != '/')
+         //path is local
+      params.logfile = current_path() + "/" + params.logfile;
+
+
+//read template
+
+  string fname_template = params.templatefile;
+
+  if (params.templatefile != "NOTEMPLATEFILE" && ! params.templatefile.empty())
+//  if (! params.templatefile.empty())
+     if (params.templatefile[0] != '/') //path is not absolute
+       {
+        fname_template = "/etc/logfilegen/templates/" + params.templatefile;
+
+        if (! file_exists (fname_template))
+           fname_template = get_home_dir() + "/.config/logfilegen/templates/" + params.templatefile;
+
+        if (! file_exists (fname_template))
+           fname_template = current_path() + "/templates/" + params.templatefile;
+
+        if (! file_exists (fname_template))
+           fname_template = current_path() + "/" + params.templatefile;
+
+        if (! file_exists (fname_template))
+           {
+            cout << "No template file " << fname_template << " found, exiting" << endl;
+            return 0;
+           }
+
+       }
+
+  if (params.templatefile[0] == '/') //path is absolute
+     if (! file_exists (fname_template))
+        {
+         cout << "No template file " << fname_template << " found, exiting" << endl;
+         return 0;
+        }
+
+
+
+//  cout << "fname_template: " << fname_template << endl;
+
+  CGenCycleUnrated cycle (&params, fname_template);
+  if (cycle.init())
+     cycle.loop();
+
+  return 0;
+}
+
