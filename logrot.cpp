350c4dc26538a54b8ce41f39003db26af10cac3c
Petr Semiletov <peter.semiletov@gmail.com>
2023-01-13 22:50:12 +0300

    more log rotation code

--- logrot.cpp
+++ logrot.cpp
@@ -14,9 +14,41 @@ CLogRotator::CLogRotator (const string &fname, size_t maxfiles, size_t maxfilesi
   cout << "max_log_files: " << max_log_files << endl;
   cout << "max_log_file_size: " << max_log_file_size << endl;
 
+  for (size_t i = 0; i < max_log_files; i++)
+      {
+       string f = source_filename + "." + std::to_string(i);
+       filenames.push_back (f);
+       cout << "f: " << f << endl;
+
+      }
+
 }
 
 
+void CLogRotator::rotate()
+{
+  cout << "CLogRotator::rotate()" << endl;
+
+
+  for (size_t i = filenames.size() - 1; i > 0; i--)
+      {
+//      string f = source_filename + "." + std::to_string(i);
+        string oldname = filenames[i-1];
+        string newname = filenames[i];
+
+        cout << "rename: " << oldname << " to: " << newname << endl;
+
+        rename (oldname.c_str(), newname.c_str());
+      }
+
+  string fname = source_filename + ".0";
+  rename (source_filename.c_str(), fname.c_str());
+
+}
+
+
+
+/*
 void CLogRotator::rotate()
 {
   cout << "CLogRotator::rotate()" << endl;
@@ -30,7 +62,11 @@ void CLogRotator::rotate()
   //test
   rename (source_filename.c_str(), fname.c_str());
 
-  /*
+
+
+
+
+
   cout << "fname: " << fname << endl;
 
   if (! file_exists (fname)) //no rotated files
@@ -41,9 +77,9 @@ void CLogRotator::rotate()
 
        return;
       }
-*/
 
-    /*
+
+
    cout << "MORE THAN 0" << endl;
 
   filenames.push_back (fname);
@@ -74,7 +110,5 @@ void CLogRotator::rotate()
        cout << "fname_new: " << fname_new << endl;
 
       }
-
-*/
 }
-
+*/
