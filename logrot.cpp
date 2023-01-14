a9c1b308c9169b43ca68d1541c4e2bf84c4af024
Petr Semiletov <peter.semiletov@gmail.com>
2023-01-14 11:49:18 +0300

    initial logrotate done

--- logrot.cpp
+++ logrot.cpp
@@ -5,14 +5,14 @@
 
 CLogRotator::CLogRotator (const string &fname, size_t maxfiles, size_t maxfilesize)
 {
-  cout << "CLogRotator::CLogRotator" << endl;
+  //cout << "CLogRotator::CLogRotator" << endl;
   source_filename = fname;
   max_log_files = maxfiles;
   max_log_file_size = maxfilesize;
 
-  cout << "source_filename: " << source_filename << endl;
-  cout << "max_log_files: " << max_log_files << endl;
-  cout << "max_log_file_size: " << max_log_file_size << endl;
+  //cout << "source_filename: " << source_filename << endl;
+//  cout << "max_log_files: " << max_log_files << endl;
+  //cout << "max_log_file_size: " << max_log_file_size << endl;
 
   for (size_t i = 0; i < max_log_files; i++)
       {
