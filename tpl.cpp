719b5dfd69b4c9d679158c94e261173ff8763adf
Petr Semiletov <peter.semiletov@gmail.com>
2023-01-31 09:43:08 +0300

    + DESTDIR support for makefiles

--- tpl.cpp
+++ tpl.cpp
@@ -60,40 +60,23 @@ CTpl::CTpl (const string &fname, const string &amode)
   if (mode == "nginx")
      {
       vars.insert (std::make_pair ("$logstring", new CVar ("$logstring", logstrings["nginx"])));
-
       vars.insert (std::make_pair ("$body_bytes_sent", new CVar ("$body_bytes_sent", "1..9999")));
       vars.insert (std::make_pair ("$connection_time", new CVar ("$connection_time", "0.001..60.000")));
       vars.insert (std::make_pair ("$document_uri", new CVar ("$document_uri", "/|/favico.ico|/doc")));
-
       vars.insert (std::make_pair ("$https", new CVar ("$https", "on|")));
-
-
       vars.insert (std::make_pair ("$http_referer", new CVar ("$http_referer", "-")));
-
-
-
       vars.insert (std::make_pair ("$http_user_agent", new CVar ("$http_user_agent", "Mozilla|Chrome|Vivaldi|Opera")));
-
       vars.insert (std::make_pair ("$is_args", new CVar ("$is_args", "?|")));
-
-
-
-//      vars.insert (std::make_pair ("$seconds_random", new CVar ("$seconds_random", "0.001..60.000")));
-
       vars.insert (std::make_pair ("$pipe", new CVar ("$pipe", "p|.")));
-
       vars.insert (std::make_pair ("$protocol", new CVar ("$protocol", " TCP/UDP")));
       vars.insert (std::make_pair ("$remote_addr", new CVar ("$remote_addr", "@ip")));
       vars.insert (std::make_pair ("$remote_user", new CVar ("$remote_user", "@str:8|@int:8")));
       vars.insert (std::make_pair ("$request", new CVar ("$request", "GET|POST|PUT|PATCH|DELETE")));
       vars.insert (std::make_pair ("$request_completion", new CVar ("$request_completion", "ok|")));
-
       vars.insert (std::make_pair ("$request_id", new CVar ("$request", "@hex:16")));
       vars.insert (std::make_pair ("$request_time", new CVar ("$request_time", "0.001..60.000")));
       vars.insert (std::make_pair ("$server_protocol", new CVar ("$server_protocol", "HTTP/1.0|HTTP/1.1|HTTP/2.0")));
-
       vars.insert (std::make_pair ("$scheme", new CVar ("$scheme", "http|https")));
-
       vars.insert (std::make_pair ("$status", new CVar ("$status", "200|400")));
       vars.insert (std::make_pair ("$time_iso8601", new CVar ("$time_iso8601", "@datetime:%Y-%m-%dT%H:%M:%SZ"))); //don't redefine
       vars.insert (std::make_pair ("$time_local", new CVar ("$time_local", "@datetime:%d/%b/%Y:%H:%M:%S %z")));
